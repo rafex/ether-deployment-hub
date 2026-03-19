385fff317dfff25afd5c2a3df13e974f0c9e14ce
rafex <rafex@rafex.dev>
2026-03-19 01:28:19 -0600

    Publish new Ether modules and database submodules

--- ether-database-core/ether-database-core/src/main/java/dev/rafex/ether/database/core/DatabaseAccessException.java
+++
@@ -1,8 +0,0 @@
-package dev.rafex.ether.database.core;
-
-public class DatabaseAccessException extends RuntimeException {
-
-	public DatabaseAccessException(final String message, final Throwable cause) {
-		super(message, cause);
-	}
-}
--- ether-database-core/ether-database-core/src/main/java/dev/rafex/ether/database/core/TransactionRunner.java
+++
@@ -1,6 +0,0 @@
-package dev.rafex.ether.database.core;
-
-public interface TransactionRunner {
-
-	<T> T inTransaction(TransactionCallback<T> callback);
-}
